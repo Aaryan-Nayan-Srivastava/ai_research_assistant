3c2c68d50e08390305cfa6d15a5b1c02b2dfa7af
aaryan <anayansrivastava@gmail.com>
2026-06-07 16:03:39 +0530

    Added the logger and config file

---
+++ app/utils/config.py
@@ -0,0 +1,14 @@
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    APP_NAME: str
+    LOG_LEVEL: str
+    GROQ_API_KEY: str
+    MODEL_NAME: str
+
+    class Config:
+        env_file = ".env"
+
+
+settings = Settings()
